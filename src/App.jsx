ca19f4db885132d2aed552268b28a11d5f7633ea
Luis Alamos <luis@patina.la>
2023-06-05 08:42:47 -0400

    testing deploy

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>Otro cambio para probar el deploy</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
